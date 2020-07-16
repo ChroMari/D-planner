7e98c5b6dc729e87af0207b32938775ffc0dcd8c
ChroMari <chromarija@gmail.com>
2020-07-16 15:20:39 +0300

    Добавление списка из задач

--- js/main.js
+++ js/main.js
@@ -13,3 +13,40 @@ function clock () {
 }
 
 setInterval(clock, 1000); //вызываем функцию, каждую секунду
+
+
+const addItems = document.querySelector('.todo-check');
+const itemsList = document.querySelector('.todo-task');
+const items = [];
+
+//функция, которая добавляет новые данные в объект, который потом сохраняется на сервере
+function addItem(e){
+  e.preventDefault(); //Метод preventDefault () интерфейса Event сообщает User agent, что если событие не обрабатывается явно, его действие по умолчанию не должно выполняться так, как обычно.
+  const text = (this.querySelector('[name=item]')).value; // помещаем данные из вводного поля и преобразуем в строку
+  //объект который содержит сведения и состояние флажка
+  const item = {
+    text,
+    done: false
+  };
+
+  items.push(item); 
+  populateList(items, itemsList); // вызываем функцию, которая будет собирать строку в парсер, который потом будет выводится
+  this.reset(); // очищаем поле ввода
+}
+
+// функция, которая будет создавать данные, которые будут добавляться на html страницу
+//plates = [] если данных не было, будет использован пустой массив
+function populateList(plates = [], platesList){
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `
+  }).join('');
+  //взависмисоти от done выставляем или нет флажок, также связка делается на i
+}
+
+
+addItems.addEventListener('submit', addItem);
