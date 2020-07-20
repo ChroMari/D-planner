ff943d780aad2be315962ffb137163271602483c
ChroMari <chromarija@gmail.com>
2020-07-20 21:15:34 +0300

    Исправила добавление задачи в JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+//---------------------------------------------Вывод даты и времени на экран---------------------------------------------------------
+
 var h_m_s = document.querySelector(".location__time");
 var d_m = document.querySelector(".location__date");
 
 var month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 var day = ['Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб', 'Вос'];
+
+var todoItems = document.querySelector('.todo-item');
+var mastItem = document.querySelector('.time-task');
+var btnItems = [];
 
 function clock () {
   const date = new Date();
@@ -10,11 +16,14 @@ function clock () {
 
   //вывод дня недели даты и месяца
   d_m.innerHTML = day[date.getDay()] + " " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+
 }
 
 setInterval(clock, 1000); //вызываем функцию, каждую секунду
 
 
+//----------------------------------------Формирование To-do листа--------------------------------------------------------------------
+/*
 const addItems = document.querySelector('.todo-check');
 const itemsList = document.querySelector('.todo-task');
 const items = [];
@@ -39,14 +48,40 @@ function addItem(e){
 function populateList(plates = [], platesList){
   platesList.innerHTML = plates.map((plate, i) => {
     return `
-      <li>
-        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+      <li class="todo-item">
+        <p class="task-btn"> Start </p>
         <label for="item${i}">${plate.text}</label>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <p class="task-time">00-00</p>
       </li>
     `
   }).join('');
   //взависмисоти от done выставляем или нет флажок, также связка делается на i
+
 }
 
-
 addItems.addEventListener('submit', addItem);
+
+*/
+
+const addItem = document.querySelector('.todo-check'); // Форма, которая добавляет задачу в список.
+const taskUl = document.querySelector('.todo-task'); // Список ul, в который будут добавляться новые задачи.
+
+addItem.addEventListener('submit', function (e) {
+
+  e.preventDefault(); // Отмена стандартного поведения. Нужно чтобы после отправки формы страница не перезагружалась.
+
+  const text = document.querySelector('.text').value; // Берём значение (текст), который записан в текстовом поле формы.
+
+  const taskHTML = `
+    <li class="todo-item">
+        <p class="task-btn"> Start </p>
+        <label>${text}</label>
+        <input type="checkbox"/>
+        <p class="task-time">00-00</p>
+      </li>
+  `; // Переменная, которая содержит базовую разметку для добавления задачи.
+
+  taskUl.insertAdjacentHTML('beforebegin', taskHTML); // insertAdjacentHTML(куда доабвлять, что добавлять) 
+
+}); // Отслеживаем событие отправки формы. addEventListener(событие которое остслеживаем, вызыв функции на это событие);
